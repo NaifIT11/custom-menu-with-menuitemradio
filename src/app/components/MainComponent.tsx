9f87993167977bf6435f255a6f9cb66715d71a16
Naif Otibi <nn.n@outlook.cl>
2024-10-28 09:44:31 +0300

    positioning the menu

--- src/app/components/MainComponent.tsx
+++ src/app/components/MainComponent.tsx
@@ -7,22 +7,22 @@ import { Check } from "lucide-react"
 
 export default function MainComponent(){
     return (
-        <div className="relative p-4">
+        <div className="relative p-4 border">
             <button aria-controls="sort-menu" aria-haspopup="menu" aria-expanded="false" className="px-4 py-2 rounded bg-slate-100 text-blue-500 font-medium">sort by</button>
-            <ul role="menu" className="p-4 bg-slate-100 text-blue-500 rounded">
-                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+            <ul role="menu" className="absolute left-[90px] z-[9999] p-4 bg-slate-100 text-blue-500 rounded">
+                <li role="menuitemradio" aria-checked="false" className="cursor-pointer flex items-center gap-3">
                     <Check className="w-4 h-4" />
                     <span>Price</span>
                 </li>
-                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+                <li role="menuitemradio" aria-checked="false" className="cursor-pointer flex items-center gap-3">
                     <Check className="w-4 h-4" />
                     <span>Popularity</span>
                 </li>
-                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+                <li role="menuitemradio" aria-checked="false" className="cursor-pointer flex items-center gap-3">
                     <Check className="w-4 h-4" />
                     <span>Date</span>
                 </li>
-                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+                <li role="menuitemradio" aria-checked="false" className="cursor-pointer flex items-center gap-3">
                     <Check className="w-4 h-4" />
                     <span>Rating</span>
                 </li>
