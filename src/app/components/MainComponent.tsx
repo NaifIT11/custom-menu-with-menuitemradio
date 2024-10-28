63587d7f10e6210793aa45fe3e373dcf28cdf995
Naif Otibi <nn.n@outlook.cl>
2024-10-28 09:36:20 +0300

    adding the menu group of radio menu

--- src/app/components/MainComponent.tsx
+++ src/app/components/MainComponent.tsx
@@ -7,6 +7,9 @@ export default function MainComponent(){
     return (
         <div className="relative p-4">
             <button aria-controls="sort-menu" aria-haspopup="menu" aria-expanded="false" className="px-4 py-2 rounded bg-slate-100 text-blue-500 font-medium">sort by</button>
+            <ul role="menu" className="p-4 bg-slate-100 text-blue-500 overflow-y-scroll">
+
+            </ul>
         </div>
     )
 }
