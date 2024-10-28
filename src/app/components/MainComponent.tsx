c7f9ad5431d8cb3bf59dd14eb0e0afd1d2ad6766
Naif Otibi <nn.n@outlook.cl>
2024-10-28 09:40:30 +0300

    adding menu item

--- src/app/components/MainComponent.tsx
+++ src/app/components/MainComponent.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { Check } from "lucide-react"
+
 
 
 
@@ -7,8 +9,23 @@ export default function MainComponent(){
     return (
         <div className="relative p-4">
             <button aria-controls="sort-menu" aria-haspopup="menu" aria-expanded="false" className="px-4 py-2 rounded bg-slate-100 text-blue-500 font-medium">sort by</button>
-            <ul role="menu" className="p-4 bg-slate-100 text-blue-500 overflow-y-scroll">
-
+            <ul role="menu" className="p-4 bg-slate-100 text-blue-500 rounded">
+                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+                    <Check className="w-4 h-4" />
+                    <span>Price</span>
+                </li>
+                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+                    <Check className="w-4 h-4" />
+                    <span>Popularity</span>
+                </li>
+                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+                    <Check className="w-4 h-4" />
+                    <span>Date</span>
+                </li>
+                <li role="menuitemradio" aria-checked="false" className="flex items-center gap-3">
+                    <Check className="w-4 h-4" />
+                    <span>Rating</span>
+                </li>
             </ul>
         </div>
     )
