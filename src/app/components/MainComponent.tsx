5df34497c9803a7322a82ad1ac00d4a866fdcc2a
Naif Otibi <nn.n@outlook.cl>
2024-10-28 09:33:02 +0300

    adding the trigger

--- src/app/components/MainComponent.tsx
+++ src/app/components/MainComponent.tsx
@@ -6,7 +6,7 @@
 export default function MainComponent(){
     return (
         <div className="relative p-4">
-            
+            <button aria-controls="sort-menu" aria-haspopup="menu" aria-expanded="false" className="px-4 py-2 rounded bg-slate-100 text-blue-500 font-medium">sort by</button>
         </div>
     )
 }
